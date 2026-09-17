8421ab3cf2d1cb6dec39dffff684ee726fd25556
Wiley Wiggins <weevil@wileywiggins.com>
2026-09-17 11:13:32 -0400

    bootstrapping, no version number
    ### Changed
    - trying a method with dash svg fills and offsets and a separate regular html slider

---
+++ journal/2026-09-17-separate-slider.md
@@ -0,0 +1,17 @@
+---
+date: 2026-09-15
+author: Wiley Wiggins
+project: "Separate slider"
+---
+
+Alright, here is proof of concept for the stroke-dasharray animating method. There's a separate, visible range input as the control. There's two svg paths, one is a track/background and the other is a fill, so it's a thermometer fill, basically.
+
+[getTotalLength() method](https://developer.mozilla.org/en-US/docs/Web/API/SVGGeometryElement/getTotalLength)
+
+SVG measures stuff in abstract "[user units](https://www.w3.org/TR/SVG/coords.html#Units)". `getTotalLength()` measures the path in units and strokeDasharray = len makes the dash pattern one single dash as long as the whole path, followed by an equally long gap that gets offset.
+
+I don't totally understand how the normalization works, but `t` is the fraction of the way along the track with 0 at one end and 1 at the other. I found lots of examples of people doing pretty wild svg animations using this trick.
+
+One thing about this method is that if there's a hiccup in page load it can throw it off, so if I add custom fonts it will need to capture `len` again. Also the units that svg use are relative to a parent, so the transforms that inkscape was adding to everything were what made a lot of this so hard. I managed to 'purify' these paths of a bunch of extra junk added by vector drawing apps & should be able to update it with whatever path I make and be ok. (The robo-barfiness of most svg is stuff added by illustrator etc, it can actually be quite clean and readable without it). The unit stuff is still a little brittle though, I noticed that it broke it to put it in a flex element for some reason but the grid element its in now didn't. Something to remember.
+
+Going to play with this a bit and think about how it would feel with a whole hand. Keeping separate sliders would give me an easy place to put the line names and I could have values...
--- slip_slider/slider.js
+++ slip_slider/slider.js
@@ -1,69 +1,15 @@
-let m = {x:50,y:50};// mouse position
-let drag = false;
-let pathLength = thePath.getTotalLength();
-let pos = pathLength / 2;
-let val = range.value;
-let oPos = thePath.getPointAtLength(pos);
-let d={};
+{
+  const fill  = document.getElementById("fill");
+  const range = document.getElementById("rangeslider");
 
-thumb.addEventListener("mousedown",onStart);
-thumb.addEventListener("touchstart",onStart);
+  let len = fill.getTotalLength();
+  fill.style.strokeDasharray = len;
 
-svg.addEventListener("mousemove",onMove);
-svg.addEventListener("touchmove",onMove);
+  const render = function () {
+    const t = (range.value - range.min) / (range.max - range.min); // 0 to 1
+    fill.style.strokeDashoffset = len * (1 - t);
+  };
 
-svg.addEventListener("mouseup",onEnd);
-svg.addEventListener("mouseleave",onEnd);
-svg.addEventListener("touchend",onEnd);
-
-function onMove(e){
-   //para prevenir el desplazamiento de pantalla
-   e.preventDefault();
-  if(drag){
-    m = oMousePosSVG(e);
-
-    pos = map5(m.x, 0, 100, 0, pathLength, true);
-    val = constrain(m.x, 0, 100); 
-    oPos = thePath.getPointAtLength(pos);
-    range.value = val;
-    thumb.setAttributeNS(null,"cx",oPos.x);
-    thumb.setAttributeNS(null,"cy",oPos.y);
-    txt.setAttributeNS(null,"x",oPos.x);
-    txt.setAttributeNS(null,"y",oPos.y);
-    txt.textContent = parseInt(val);
-    
-  }
-}
-function onStart(e){m = oMousePosSVG(e);                      
-  drag = true;                
-}
-function onEnd(e){drag = false;}
-
-
-//a p5.js function
-function map5(n, a, stop1, start2, stop2, withinBounds) {
-  var newval = (n - a) / (stop1 - a) * (stop2 - start2) + start2;
-  if (!withinBounds) {
-    return newval;
-  }
-  if (start2 < stop2) {
-    return this.constrain(newval, start2, stop2);
-  } else {
-    return this.constrain(newval, stop2, start2);
-  }
-};
-
-function constrain(n, low, high) {
-  return Math.max(Math.min(n, high), low);
-};
-
-
-function oMousePosSVG(ev) {
-  let e = ev.touches ? ev.touches[0] : ev;
-      var p = svg.createSVGPoint();
-      p.x = e.clientX;
-      p.y = e.clientY;
-      var ctm = svg.getScreenCTM().inverse();
-      var p =  p.matrixTransform(ctm);
-      return p;
+  range.addEventListener("input", render);
+  render();
 }
