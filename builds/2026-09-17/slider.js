b0a2ba4a6a7e167becbe659220a5cd54f26c42e6
Wiley Wiggins <weevil@wileywiggins.com>
2026-09-19 23:17:18 -0400

    bootstrapping, no version
    ### Added
    - builds folder with dated versions

---
+++ builds/2026-09-17/slider.js
@@ -0,0 +1,17 @@
+{
+  const fill    = document.getElementById("fill");
+  const range   = document.getElementById("rangeslider");
+  const readout = document.getElementById("readout");
+
+  let len = fill.getTotalLength();
+  fill.style.strokeDasharray = len;
+
+  const render = function () {
+    const t = (range.value - range.min) / (range.max - range.min); // 0 to 1
+    fill.style.strokeDashoffset = len * (1 - t);
+    readout.textContent = range.valueAsNumber;
+  };
+
+  range.addEventListener("input", render);
+  render();
+}
